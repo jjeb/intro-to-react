027d3515073b8f4e9909e22118f5ca2d3bb0769e
Juan Esparza <juanj.esparza@gmail.com>
2019-04-19 16:01:15 -0700

    JXS and some more tooling

--- src/app.js
+++ src/app.js
@@ -8,28 +8,36 @@ class App extends React.Component {
   }
 
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Adopt Me!"
-      ),
-      React.createElement(Pet, {
-        name: "luna",
-        animal: "dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Sun",
-        animal: "Cat",
-        breed: "mixed"
-      }),
-      React.createElement(Pet, {
-        name: "Sea",
-        animal: "Cat",
-        breed: "Mixed"
-      })
-    ]);
+    //   return React.createElement("div", {}, [
+    //     React.createElement(
+    //       "h1",
+    //       { onClick: this.handleTitleClick },
+    //       "Adopt Me!"
+    //     ),
+    //     React.createElement(Pet, {
+    //       name: "luna",
+    //       animal: "dog",
+    //       breed: "Havanese"
+    //     }),
+    //     React.createElement(Pet, {
+    //       name: "Sun",
+    //       animal: "Cat",
+    //       breed: "mixed"
+    //     }),
+    //     React.createElement(Pet, {
+    //       name: "Sea",
+    //       animal: "Cat",
+    //       breed: "Mixed"
+    //     })
+    //   ]);
+    return (
+      <div>
+        <h1>Adopt Me</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese" />
+        <Pet name="Sun" animal="Cat" breed="mixed" />
+        <Pet name="Sea" animal="Cat" breed="Mixed" />
+      </div>
+    );
   }
 }
 
