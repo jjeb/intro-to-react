5e1e385d4782966a5e806168e02a39dd3b5da4b4
Juan Esparza <juanj.esparza@gmail.com>
2019-05-24 21:13:50 -0700

    Work In Progress, refactoring the Details page

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,49 @@
 import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router/lib/history";
+
+const petFinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    petFinder.pet
+      .get({ output: "full", id: this.props.id })
+      .then(data => {
+        let breed;
+        const pet = data.petfinder;
+
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+
+        this.setState({
+          name: pet.name,
+          animal: pet.aniaml,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      // .catch(() => {
+      //   navigate("/");
+      // });
+  }
+
   render() {
     return <h1>lol</h1>;
   }
